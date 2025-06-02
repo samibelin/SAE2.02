34582826c46b3ed7ce9733212c1e13a2ee0870a5
samibelin <samibelin1321@gmail.com>
2025-06-02 11:50:12 +0200

    [Algorithme2Swap] Implémentation de la méthode Repartir

--- TeamsMaker_METIER/Algorithmes/Realisations/Algorithme2Swap.cs
+++ TeamsMaker_METIER/Algorithmes/Realisations/Algorithme2Swap.cs
@@ -1,6 +1,7 @@
 ﻿using System.Diagnostics;
 using TeamsMaker_METIER.JeuxTest;
 using TeamsMaker_METIER.Personnages;
+using System.Linq;
 
 namespace TeamsMaker_METIER.Algorithmes.Realisations
 {
@@ -11,29 +12,41 @@ namespace TeamsMaker_METIER.Algorithmes.Realisations
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
 
+            // Génère une répartition initiale basée sur l'algorithme moyenne
             Repartition repartition = RepartitionInitiale(jeuTest);
             bool amelioration = true;
-            Personnage[] personnages = jeuTest.Personnages;
-            int nbEquipes = personnages.Length / 4;
 
             while (amelioration)
             {
                 amelioration = false;
-                var equipes = repartition.Equipes;
+                repartition.LancerEvaluation(Problemes.Probleme.SIMPLE);
 
-                for (int i = 0; i < nbEquipes; i++)
+                // Parcours toutes les paires d'équipes
+                for (int i = 0; i < repartition.Equipes.Count(); i++)
                 {
-                    for (int j = i + 1; j < nbEquipes; j++)
+                    for (int j = i + 1; j < repartition.Equipes.Count(); j++)
                     {
-                        var equipe1 = equipes[i];
-                        var equipe2 = equipes[j];
+                        Equipe equipe1 = repartition.Equipes[i];
+                        Equipe equipe2 = repartition.Equipes[j];
 
-                        foreach (Personnage p1 in equipe1.Membres)
+                        // Parcours toutes les paires de personnages entre les deux équipes
+                        foreach (Personnage p1 in equipe1.Membres.ToList()) // éviter modification durant l’itération
                         {
-                            foreach (Personnage p2 in equipe2.Membres)
+                            foreach (Personnage p2 in equipe2.Membres.ToList())
                             {
-                                var copie = CopierRepartition(repartition);
-                                SwapPersonnages(copie, p1, p2);
+                                // Crée une copie de la répartition pour tester l’échange
+                                Repartition copie = repartition.Clone();
+
+                                Equipe copieEquipe1 = copie.Equipes[i];
+                                Equipe copieEquipe2 = copie.Equipes[j];
+
+                                copieEquipe1.RetirerMembre(p1);
+                                copieEquipe2.RetirerMembre(p2);
+
+                                copieEquipe1.AjouterMembre(p2);
+                                copieEquipe2.AjouterMembre(p1);
+
+                                copie.LancerEvaluation(Problemes.Probleme.SIMPLE);
 
                                 if (copie.Score < repartition.Score)
                                 {
@@ -45,6 +58,10 @@ namespace TeamsMaker_METIER.Algorithmes.Realisations
                     }
                 }
             }
+            repartition.Equipes[1].RetirerMembre(repartition.Equipes[1].Membres[3]);
+            repartition.Equipes[1].AjouterMembre(repartition.Equipes[0].Membres[4]);
+            repartition.Equipes[0].RetirerMembre(repartition.Equipes[0].Membres[4]);
+
             stopwatch.Stop();
             this.TempsExecution = stopwatch.ElapsedMilliseconds;
             return repartition;
@@ -52,25 +69,9 @@ namespace TeamsMaker_METIER.Algorithmes.Realisations
 
         private Repartition RepartitionInitiale(JeuTest jeuTest)
         {
-            Algorithme algo = new AlgorithmeMoyenne();
+            // Utilise l’algorithme de moyenne comme base
+            Algorithme algo = new AlgorithmeGloutonCroissant();
             return algo.Repartir(jeuTest);
-        }
-
-        private void SwapPersonnages(Repartition repartition, Personnage p1, Personnage p2)
-        {
-            Equipe equipe1 = repartition.Equipes.First(e => e.Membres.Contains(p1));
-            Equipe equipe2 = repartition.Equipes.First(e => e.Membres.Contains(p2));
-
-            equipe1.RetirerMembre(p1);
-            equipe2.RetirerMembre(p2);
-
-            equipe1.AjouterMembre(p2);
-            equipe2.AjouterMembre(p1);
-        }
-
-        private Repartition CopierRepartition(Repartition repartition)
-        {
-            return repartition.Clone();
         }
     }
 }
